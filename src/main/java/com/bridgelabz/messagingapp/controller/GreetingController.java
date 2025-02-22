0aa3cbb0729e7439466fa2b43a3f6092c4abe8c2
Saurabh Saraswat <saurabh45215@gmail.com>
2025-02-22 12:49:53 +0530

    [Saurabh] Add Hello JSON Methods

--- src/main/java/com/bridgelabz/messagingapp/controller/GreetingController.java
+++ src/main/java/com/bridgelabz/messagingapp/controller/GreetingController.java
@@ -1,27 +1,39 @@
 package com.bridgelabz.messagingapp.controller;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import org.json.JSONObject;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 public class GreetingController {
+    JSONObject jsonObject = new JSONObject();
+    GreetingController(){
+        jsonObject.put("name","default_value");
+    }
     //Get Mapping
-    @GetMapping("/sayhello")
+    @GetMapping(value = "/hello",produces = "application/json")
     public String sayHello(){
-        return "Hello from Shit";
+        return jsonObject.toString();
     }
     //Post Mapping
-    @PostMapping("/hello")
-    public String postHello(@RequestBody String name){
-        return "hello for "+ name + " from Post";
+    @PostMapping(value = "/hello",produces = "application/json")
+    public String postHello(@RequestBody Map<String,Object> newData){
+        jsonObject.put("name",newData.get("name"));
+        return "hello for "+jsonObject.get("name")+ " from Post";
     }
     //Put Mapping
-    @PutMapping("/hello")
-    public String putHello(@RequestBody String name){
-        return "hello for "+ name +" from Put";
+    @PutMapping(value = "/hello",produces = "application/json")
+    public String putHello(@RequestBody Map<String,Object> newData){
+        jsonObject.put("name",newData.get("name"));
+        return "hello for "+ jsonObject.get("name") +" from Put";
     }
     //Delete Mapping
-    @DeleteMapping("/deletehello")
-    public String deleteHello(@RequestBody String name){
-        return "hello "+name+" deleted";
+    @DeleteMapping(value = "/hello",produces = "application/json")
+    public String deleteHello(@RequestBody Map<String,Object> newData){
+        return "hello "+newData.remove("name")+" deleted";
     }
 }
