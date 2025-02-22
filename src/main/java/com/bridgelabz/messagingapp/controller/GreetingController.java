b9d21769055b9bbcef9182c05365c4309a4bf15a
Saurabh Saraswat <saurabh45215@gmail.com>
2025-02-22 11:47:30 +0530

    [Saurabh] Add Hello Methods

---
+++ src/main/java/com/bridgelabz/messagingapp/controller/GreetingController.java
@@ -0,0 +1,27 @@
+package com.bridgelabz.messagingapp.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class GreetingController {
+    //Get Mapping
+    @GetMapping("/sayhello")
+    public String sayHello(){
+        return "Hello from Shit";
+    }
+    //Post Mapping
+    @PostMapping("/hello")
+    public String postHello(@RequestBody String name){
+        return "hello for "+ name + " from Post";
+    }
+    //Put Mapping
+    @PutMapping("/hello")
+    public String putHello(@RequestBody String name){
+        return "hello for "+ name +" from Put";
+    }
+    //Delete Mapping
+    @DeleteMapping("/deletehello")
+    public String deleteHello(@RequestBody String name){
+        return "hello "+name+" deleted";
+    }
+}
